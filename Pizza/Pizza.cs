559cb491db45f5118795f01f194c9807648cbf39
Diak <perikdiak@petrik.hu>
2026-02-17 13:06:32 +0100

    pizza

--- Pizza/Pizza.cs
+++ Pizza/Pizza.cs
@@ -8,6 +8,22 @@ namespace Pizza
 {
 	internal class Pizza
 	{
-		
+
+		private string nev;
+		private int meret;
+
+		public Pizza(string nev, int meret)
+		{
+			this.nev = nev;
+			this.meret = meret;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Meret { get => meret; set => meret = value; }
+
+		public override string ToString()
+		{
+			return $"{nev} ({meret} cm)";
+		}
 	}
 }
