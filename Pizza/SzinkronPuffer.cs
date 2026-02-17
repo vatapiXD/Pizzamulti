f0247919aa10bdf01162431b4d5d169a78cd256a
Diak <perikdiak@petrik.hu>
2026-02-17 13:08:28 +0100

    kuffer

--- Pizza/SzinkronPuffer.cs
+++ Pizza/SzinkronPuffer.cs
@@ -2,11 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Pizza
 {
-	internal class SzinkronPuffer
+	internal class SzinkronPuffer<T>
 	{
+		private Queue<T> sor;
+		private int meret;
+		private readonly object zar = new object();
+
+		public SzinkronPuffer(int meret)
+		{
+			this.meret = meret;
+			sor = new Queue<T>();
+		}
+
+		public void Hozzaad(T elem)
+		{
+			lock (zar)
+			{
+				while (sor.Count == meret)
+				{
+					Monitor.Wait(zar);
+				}
+
+				sor.Enqueue(elem);
+				Monitor.PulseAll(zar);
+			}
+		}
+
+		public T Kivesz()
+		{
+			lock (zar)
+			{
+				while (sor.Count == 0)
+				{
+					Monitor.Wait(zar);
+				}
+
+				T elem = sor.Dequeue();
+				Monitor.PulseAll(zar);
+				return elem;
+			}
+		}
 	}
+	
+
 }
