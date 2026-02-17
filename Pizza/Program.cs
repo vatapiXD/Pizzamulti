cc02773644d606e7e90cc104559da2f0890f24ec
Diak <perikdiak@petrik.hu>
2026-02-17 13:13:38 +0100

    prog

--- Pizza/Program.cs
+++ Pizza/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Pizza
@@ -10,15 +11,19 @@ namespace Pizza
 	{
 		static void Main(string[] args)
 		{
+			PizzaKemence kemence = new PizzaKemence(5);
 
+			Sef sef1 = new Sef(kemence);
+			Sef sef2 = new Sef(kemence);
+			Futar futar = new Futar(kemence);
 
+			Thread t1 = new Thread(sef1.Futtat);
+			Thread t2 = new Thread(sef2.Futtat);
+			Thread t3 = new Thread(futar.Futtat);
 
-
-
-
-
-
-
+			t1.Start();
+			t2.Start();
+			t3.Start();
 		}
 	}
 }
